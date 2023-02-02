2b984704337e9fa8b05b9411a5ab1fc0b0567e50
deli-off <dzamsedovnazif1@gmail.com>
2023-02-02 22:14:14 +0500

    finished

--- createlelmt/createlelmt/script.js
+++ createlelmt/createlelmt/script.js
@@ -14,6 +14,11 @@ let todos = [{
 
 let form = document.forms.add_task_form;
 let container = document.querySelector(".container");
+let modal = document.querySelector('.modal')
+let modalBG = document.querySelector('.modal__bg')
+let closeBtn = document.querySelector('.close-btn')
+let input = document.querySelector('.modal__input')
+let change = document.querySelector('.modal__btn')
 
 form.onsubmit = (event) => {
 	event.preventDefault();
@@ -43,6 +48,7 @@ const reload = (arr) => {
 	for (let item of arr) {
 		let box = document.createElement("div");
 		let img = document.createElement("img");
+		let img2 = document.createElement("img")
 		let h3 = document.createElement("h3");
 		let span = document.createElement("span");
 
@@ -50,13 +56,17 @@ const reload = (arr) => {
 		img.classList.add("img");
 		h3.classList.add("h3");
 		span.classList.add("span");
+		img2.classList.add("img2")
 
 		h3.innerHTML = item.task;
-		img.src = "./close.svg";
+		img.src = "./img/close.svg";
+		img2.src = "./img/edit-svgrepo-com.svg";
+		img2.setAttribute('width', '20px')
+		img2.setAttribute('height', '20px')
 		span.innerHTML = item.time;
 
 		container.append(box);
-		box.append(img, h3, span);
+		box.append(img, h3, span, img2);
 
 		if (item.completed === true) {
 			h3.classList.add('box-line')
@@ -73,8 +83,49 @@ const reload = (arr) => {
 			}
 		}
 
+		change.onclick = () => {
+			h3.innerHTML = input.value
+			modal.style.opacity = 0
+			modalBG.style.opacity = 0
+			modal.style.scale = .1
+			setTimeout(() => {
+				modal.style.display = 'none'
+				modalBG.style.display = 'none'
+			}, 200)
+		}
+
 		img.onclick = () => {
 			box.style.display = 'none'
+		}
+
+		function openClose(arr, func) {
+			arr.onclick = () => {
+				func()
+			}
+		}
+
+		openClose(img2, openModal)
+		openClose(closeBtn, closeModal)
+
+		function openModal() {
+			modal.style.opacity = 1
+			modalBG.style.opacity = 1
+			modal.style.scale = 1
+			setTimeout(() => {
+				modal.style.display = 'block'
+				modalBG.style.display = 'block'
+
+			}, 200)
+		}
+
+		function closeModal() {
+			modal.style.opacity = 0
+			modalBG.style.opacity = 0
+			modal.style.scale = .1
+			setTimeout(() => {
+				modal.style.display = 'none'
+				modalBG.style.display = 'none'
+			}, 200)
 		}
 	};
 }
